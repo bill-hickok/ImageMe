2f4ef3376f7f9dc8788599405ae046e9ea50002b
kristen howton <kristen.howton@udig.com>
2023-11-14 09:21:47 -0600

    form and tabs

--- src/components/common/form/AppDataGrid.tsx
+++ src/components/common/form/AppDataGrid.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import React from "react";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 interface DocumentGridProps {
   rows: any[]; // Replace 'any[]' with the actual type of your data
+  columns: GridColDef[];
 }
 
-const columns: GridColDef[] = [
-  { field: 'docTypeNo', headerName: 'Doc Type No.', flex: 1, headerClassName: 'super-app-theme--header', cellClassName: 'bold-cell',  },
-  { field: 'docTypeName', headerName: 'Doc Type Name', flex: 1, headerClassName: 'header-cell', cellClassName: 'bold-cell' },
-  { field: 'status', headerName: 'Status', flex: 1, headerClassName: 'header-cell', cellClassName: 'bold-cell' },
-  { field: 'keywords', headerName: 'Keywords', flex: 1, headerClassName: 'header-cell', cellClassName: 'bold-cell' },
-  { field: 'lastModified', headerName: 'Last Modified', flex: 1, headerClassName: 'header-cell', cellClassName: 'bold-cell' },
-  { field: 'modifiedBy', headerName: 'Modified by', flex: 1, headerClassName: 'header-cell', cellClassName: 'bold-cell' },
-  { field: 'dateCreated', headerName: 'Date Created', flex: 1, headerClassName: 'header-cell', cellClassName: 'bold-cell' },
-  { field: 'createdBy', headerName: 'Created by', flex: 1, headerClassName: 'header-cell', cellClassName: 'bold-cell' },
-];
-
-const AppDataGrid: React.FC<DocumentGridProps> = ({ rows }) => {
+const AppDataGrid: React.FC<DocumentGridProps> = ({ rows, columns }) => {
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid rows={rows} columns={columns} pagination/>
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid rows={rows} columns={columns} pagination />
     </div>
   );
 };
--- src/components/common/form/form.tsx
+++ src/components/common/form/form.tsx
@@ -1,12 +1,13 @@
 // ParentForm.tsx
-import React, { Children, ComponentPropsWithoutRef } from 'react';
-import { Formik, FormikHelpers, FormikValues } from 'formik';
-import * as Yup from 'yup';
-import AppTextInput from './AppTextInput';
-import AppSubmitButton from './AppSubmitButton';
-import AppRadioInput from './AppRadioInput';
-import AppTextbox from './AppTextbox';
-import AppDataGrid from './AppDataGrid';
+import React, { Children, ComponentPropsWithoutRef } from "react";
+import { Formik, FormikHelpers, FormikValues } from "formik";
+import * as Yup from "yup";
+import AppTextInput from "./AppTextInput";
+import AppSubmitButton from "./AppSubmitButton";
+import AppRadioInput from "./AppRadioInput";
+import AppTextbox from "./AppTextbox";
+import AppDataGrid from "./AppDataGrid";
+import { FormControl } from "@mui/material";
 
 // interface FormValues {
 //   name: string;
@@ -16,10 +17,10 @@ import AppDataGrid from './AppDataGrid';
 // }
 
 type FormProps = ComponentPropsWithoutRef<"form"> & {
-    handleSubmit: (values: FormikValues) => void;
-    handleChanged: (values: FormikValues) => void;
-    // initalValues: FormikValues;
-  };
+  handleSubmit: (values: FormikValues) => void;
+  handleChanged: (values: FormikValues) => void;
+  // initalValues: FormikValues;
+};
 
 // const validationSchema = Yup.object().shape({
 //   name: Yup.string().required('Name is required'),
@@ -28,7 +29,7 @@ type FormProps = ComponentPropsWithoutRef<"form"> & {
 //   description: Yup.string(),
 // });
 
-const Form = ({children, ...props}: FormProps) => {
+const Form = ({ children, ...props }: FormProps) => {
   const handleSubmit = (values: FormikValues, actions: any) => {
     // Handle form submission logic here
     console.log(values);
@@ -37,37 +38,23 @@ const Form = ({children, ...props}: FormProps) => {
 
   const handleTabChange = (value: number) => {
     // Handle tab change logic here if needed
-    console.log('Selected tab:', value);
+    console.log("Selected tab:", value);
   };
 
-  console.log(children);
   const genderOptions = [
-    { label: 'Male', value: 'male' },
-    { label: 'Female', value: 'female' },
-    { label: 'Other', value: 'other' },
-  ];
-
-  const rows = [
-    { id: 1, docTypeNo: '12345678', docTypeName: 'Contract Agreement', status: 'Active', keywords: 'Contract, Legal', lastModified: '2023-01-01', modifiedBy: 'User 1', dateCreated: '2022-01-01', createdBy: 'User 2' },
-    { id: 2, docTypeNo: '45678912', docTypeName: 'Technical Manual', status: 'Inactive', keywords: 'Technical, Guide', lastModified: '2023-02-15', modifiedBy: 'User 3', dateCreated: '2022-02-20', createdBy: 'User 4' },
-    { id: 3, docTypeNo: '78912345', docTypeName: 'Financial Report', status: 'Active', keywords: 'Finance, Report', lastModified: '2023-03-05', modifiedBy: 'User 2', dateCreated: '2022-03-10', createdBy: 'User 1' },
-    { id: 4, docTypeNo: '56789123', docTypeName: 'HR Policy', status: 'Under Review', keywords: 'Human Resources, Policy', lastModified: '2023-04-12', modifiedBy: 'User 4', dateCreated: '2022-04-22', createdBy: 'User 3' },
-    { id: 5, docTypeNo: '34567891', docTypeName: 'Project Plan', status: 'Active', keywords: 'Project, Planning', lastModified: '2023-05-18', modifiedBy: 'User 5', dateCreated: '2022-05-25', createdBy: 'User 6' },
+    { label: "Male", value: "male" },
+    { label: "Female", value: "female" },
+    { label: "Other", value: "other" },
   ];
 
   return (
     <Formik
-      initialValues={{ name: '', gender: '', hobbies: [], description: '' }}
+      initialValues={{ name: "", gender: "", hobbies: [], description: "" }}
       // validationSchema={validationSchema}
       onSubmit={handleSubmit}
     >
       <div>
-        <form>
-          <div style={{ marginTop: '100px' }}>
-              <AppDataGrid rows={rows} />
-          </div>
-          {children}
-        </form>
+        <FormControl>{children}</FormControl>
       </div>
     </Formik>
   );
